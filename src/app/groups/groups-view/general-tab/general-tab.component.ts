cde0ebf1a3ebb005e12c638fa4976e163ba64c13
Mwesigye John Bosco <johnboscomwesigye@gmail.com>
2025-09-12 06:55:55 +0300

    Route to Loan Repayment screen for both client and group dollar sign

--- src/app/groups/groups-view/general-tab/general-tab.component.ts
+++ src/app/groups/groups-view/general-tab/general-tab.component.ts
@@ -1,6 +1,6 @@
 /** Angular Imports */
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 /**
  * Groups View General Tab Component.
@@ -91,7 +91,7 @@ export class GeneralTabComponent {
    * Fetches group's related data from `resolve`
    * @param {ActivatedRoute} route Activated Route.
    */
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
     this.route.data.subscribe(
       (data: { groupAccountsData: any; groupClientMembers: any; groupSummary: any; glimData: any; gsimData: any }) => {
         this.glimAccounts = data.glimData;
@@ -128,4 +128,13 @@ export class GeneralTabComponent {
   routeEdit($event: MouseEvent) {
     $event.stopPropagation();
   }
+
+  navigateToMakeRepayment(loan: any, $event: MouseEvent) {
+    $event.stopPropagation();
+    this.router.navigate(['../', 'loans-accounts', loan.id, 'actions', 'Make Repayment'], {
+      relativeTo: this.route,
+      state: { data: loan }
+    });
+  }
+
 }
