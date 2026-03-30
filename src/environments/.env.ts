e56b1b3b0313283f3ab52edc466923600464c37a
nuwagabavictor <nuwagabavictor197@gmail.com>
2026-03-30 16:41:23 +0300

    HO-Prefix

---
+++ src/environments/.env.ts
@@ -0,0 +1,10 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export default {
+  'mifos_x': {
+    'version': '260307',
+    'hash': '335be69d'
+  },
+  'allow_switching_backend_instance': true
+};
+/* tslint:enable */
