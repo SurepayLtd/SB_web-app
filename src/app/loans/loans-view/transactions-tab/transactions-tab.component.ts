fec986a1f614fed36e3ca57e459e5b91827cf189
nuwagabavictor <nuwagabavictor197@gmail.com>
2026-04-14 19:42:13 +0300

    404 Error on Group Reversals

--- src/app/loans/loans-view/transactions-tab/transactions-tab.component.ts
+++ src/app/loans/loans-view/transactions-tab/transactions-tab.component.ts
@@ -340,12 +340,31 @@ export class TransactionsTabComponent implements OnInit {
     return !(this.isReAmortize(transactionType) || this.isReAge(transactionType));
   }
 
+  private getEntityId(): { type: 'client' | 'group'; id: string } {
+    let route = this.route.snapshot;
+
+    while (route) {
+      if (route.params['clientId']) {
+        return { type: 'client', id: route.params['clientId'] };
+      }
+      if (route.params['groupId']) {
+        return { type: 'group', id: route.params['groupId'] };
+      }
+      route = route.parent!;
+    }
+
+    throw new Error('No clientId or groupId found in route');
+  }
+
   private reload() {
-    const clientId = this.route.parent.parent.snapshot.params['clientId'];
-    const url: string = this.router.url;
-    this.router
-      .navigateByUrl(`/clients/${clientId}/loans-accounts`, { skipLocationChange: true })
-      .then(() => this.router.navigate([url]));
+    const entity = this.getEntityId();
+
+    const baseUrl =
+      entity.type === 'client' ? `/clients/${entity.id}/loans-accounts` : `/groups/${entity.id}/loans-accounts`;
+
+    const currentUrl = this.router.url;
+
+    this.router.navigateByUrl(baseUrl, { skipLocationChange: true }).then(() => this.router.navigateByUrl(currentUrl));
   }
 
   displaySubMenu(transaction: LoanTransaction): boolean {
