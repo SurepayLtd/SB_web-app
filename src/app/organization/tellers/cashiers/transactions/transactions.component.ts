a04fa5a379e198e6bccc2c4c83f37d137e381b69
nuwagabavictor <nuwagabavictor197@gmail.com>
2026-06-18 11:48:57 +0300

    Filter Cashier Transactions By Date

--- src/app/organization/tellers/cashiers/transactions/transactions.component.ts
+++ src/app/organization/tellers/cashiers/transactions/transactions.component.ts
@@ -74,14 +74,17 @@ export class TransactionsComponent implements OnInit {
    * Filters data in transactions table based on date.
    */
   filterByDate(event: any) {
-    const date = event.value;
+    const date: Date = event.value;
 
+    if (!date || !this.dataSource) {
+      return;
+    }
     const filter =
       date.getFullYear() +
       '-' +
       String(date.getMonth() + 1).padStart(2, '0') +
       '-' +
-      String(date.getDay()).padStart(2, '0');
+      String(date.getDate()).padStart(2, '0');
 
     this.dataSource.filter = filter;
   }
@@ -114,5 +117,14 @@ export class TransactionsComponent implements OnInit {
     this.dataSource = new MatTableDataSource(this.cashierData.cashierTransactions.pageItems);
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+
+    this.dataSource.filterPredicate = (data: any, filter: string) => {
+      const txnDate = this.formatTxnDate(data.txnDate);
+      return txnDate === filter;
+    };
+  }
+
+  private formatTxnDate(txnDate: number[]): string {
+    return txnDate[0] + '-' + String(txnDate[1]).padStart(2, '0') + '-' + String(txnDate[2]).padStart(2, '0');
   }
 }
